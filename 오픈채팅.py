24aeaf640ee45cfbb396afb9dbac665d13c0f24b
Yoo KyeongMi <89245114+YooKyungmi@users.noreply.github.com>
2022-01-24 17:54:53 +0900

    3 commit

--- 오픈채팅.py
+++ 오픈채팅.py
@@ -1,32 +1,28 @@
+from email import message
+
+
 record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo",
           "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]
 # ["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."
 
-# 런타임 오류
-
 
 def solution(record):
     data = {}
-    answer = []
+    ans = []
+    words = ["님이 들어왔습니다.", "님이 나갔습니다."]
     for i in range(len(record)):
-        list = record[i].split()
-        answer.append(list[1])
+        line = record[i].split()
 
-        if list[0] == "Change":
-            data[list[1]] = list[2]
-            answer.pop()
+        if line[0] == "Change":
+            data[line[1]] = line[2]
 
-        elif list[0] == "Enter":
-            data[list[1]] = list[2]
+        elif line[0] == "Enter":
+            data[line[1]] = line[2]
+            ans.append((line[1], 0))
+        else:
+            ans.append((line[1], 1))
 
-    for j in range(len(record)):
-        if record[j][0] == 'E':
-            ans = "{0}님이 들어왔습니다.".format(data[answer[j]])
-            answer[j] = ans
-
-        elif record[j][0] == 'L':
-            ans = "{0}님이 나갔습니다.".format(data[answer[j]])
-            answer[j] = ans
+    answer = list(map(lambda x: data[x[0]]+words[x[1]], ans))
 
     return answer
 
